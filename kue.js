2827443eb203b7b1f506419fb963438acdcef0ce
limmi26 <kisamakiazuel@gmail.com>
2022-01-10 10:08:49 +0700

    perubahan 2

--- kue.js
+++ kue.js
@@ -33,117 +33,41 @@ class Kue {
 
 }
 
-class kuePutu extends Kue {
-    constructor(...args,gulaLevel) {
-        super(...args),
-        this._gukaLevel = gulaLevel;
-    }
-
-    // getter
-    get bahan() {
-        return this._bahan;
-    }
-    get gula() {
-        return this._gula;
-    }
-    get asesoris() {
-        return this._asesoris;
-    }
-    get berat() {
-        return this._berat;
-    }
-    get gulaLevel() {
-        return this._gukaLevel
-    }
-
-    // setter
-    set gula(value) {
-        this._gula = value;
-    }
-    set asesoris(value) {
-        this._asesoris = value;
-    }
-    set berat(value) {
-        this._berat = value;
-    }
-    set gulaLevel(value) {
-    this._gukaLevel = value;
-    }
-}
+​class​ ​KuePutu​ ​extends​ ​Kue​{ 
+    ​    ​constructor​(​gulaLevel​,​ ...​args​)​{ 
+    ​        ​super​(​...​args​) 
+    ​        ​this​.​_gulaLevel​ ​=​ ​gulaLevel​; 
+    ​    ​} 
+    ​    ​// getter 
+    ​    ​get​ ​gulaLevel​(​)​{ 
+    ​        ​return​ ​this​.​_gulaLevel 
+    ​    ​}​; 
+    ​    ​// setter 
+    ​    ​set​ ​gulaLevel​(​value​)​{ 
+    ​        ​this​.​_gulaLevel​ ​=​ ​value​; 
+    ​    ​}​; 
+    ​}​; 
 // objek
 const kuePutu = new Kue("gula jawa","daun",10);
 console.log(kuePutu);
 
-class kueCubit extends Kue {
-    constructor(gula,asesoris,berat) {
-        this._bahan = "Telur";
-        this._gula = Gula;
-        this._asesoris = Asesoris;
-        this._berat = Berat;
-    }
-
-    // getter
-    get bahan() {
-        return this._bahan;
-    }
-    get gula() {
-        return this._gula;
-    }
-    get asesoris() {
-        return this._asesoris;
-    }
-    get berat() {
-        return this._berat;
-    }
-
-    // setter
-    set gula(value) {
-        this._gula = value;
-    }
-    set asesoris(value) {
-        this._asesoris = value;
-    }
-    set berat(value) {
-        this._berat = value;
-    }
-}
+​class​ ​KueCubit​ ​extends​ ​Kue​{ 
+    ​    ​constructor​(​...​args​)​{ 
+    ​        ​super​(​...​args​) 
+    ​        ​this​.​_bahan​ ​=​ ​[​'tepung'​,​'telur'​]​;​  
+    ​    ​} 
+    ​     
+    ​}​; 
 // objek
 const kueCubit = new Kue("gula jawa","daun",10);
 console.log(kueCubit);
 
-class kueKetawa extends Kue {
-    constructor(gula,asesoris,berat) {
-        this._bahan = "Telur";
-        this._gula = Gula;
-        this._asesoris = Asesoris;
-        this._berat = Berat;
-    }
-
-    // getter
-    get bahan() {
-        return this._bahan;
-    }
-    get gula() {
-        return this._gula;
-    }
-    get asesoris() {
-        return this._asesoris;
-    }
-    get berat() {
-        return this._berat;
-    }
-
-    // setter
-    set gula(value) {
-        this._gula = value;
-    }
-    set asesoris(value) {
-        this._asesoris = value;
-    }
-    set berat(value) {
-        this._berat = value;
-    }
-}
+​class​ ​KueKetawa​ ​extends​ ​Kue​{ 
+    ​    ​constructor​(​...​args​)​{ 
+    ​        ​super​(​...​args​) 
+    ​        ​this​.​_bahan​ ​=​ ​[​'tepung'​,​'telur'​]​; 
+    ​    ​} 
+    ​}​;
 // objek
 const kueKetawa = new Kue("gula jawa","daun",10);
 console.log(kueKetawa);
